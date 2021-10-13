5eb45c4f5d34837c4cad0c4dd8d472aae3801637
dakeXd <47038301+dakeXd@users.noreply.github.com>
2021-10-13 13:01:06 +0200

    Keep Aspect Ratio in Mobile Devices
    Black Bars

---
+++ TalesOFDumbgeon/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private int direction;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Movement.SetDirection(direction);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Movement.SetDirection(-direction);
+    }
+}
