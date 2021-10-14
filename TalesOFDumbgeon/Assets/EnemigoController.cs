6c59ec1fb4242f45b4715f3797e16b0e00e66e78
strikealex <43806315+strikealex@users.noreply.github.com>
2021-10-14 21:22:42 +0200

    Terminado las colisiones

---
+++ TalesOFDumbgeon/Assets/EnemigoController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoController : MonoBehaviour
+{
+
+    public int vida = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bala"))
+        {
+            Debug.Log(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {        
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        vida -= damage;
+    }
+
+}
