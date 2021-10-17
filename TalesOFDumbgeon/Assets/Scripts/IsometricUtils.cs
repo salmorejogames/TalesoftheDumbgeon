cf9b1cb5e3e81138b958195d1cab2f865cbab415
dakeXd <47038301+dakeXd@users.noreply.github.com>
2021-10-17 18:39:28 +0200

    Put Everything Together

---
+++ TalesOFDumbgeon/Assets/Scripts/IsometricUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public static class IsometricUtils
+{
+    public static Vector2 CartesianToIsometric(Vector2 input)
+    {
+        return new Vector2(input.x - input.y , -(-input.x - input.y )/2 );
+    }
+
+    public static Vector2 PolarisToCartesian(float angle, float distance)
+    {
+        return new Vector2(distance * (float) Math.Cos((Math.PI / 180) * angle), distance * (float )Math.Sin((Math.PI / 180)*angle));
+    }
+}
