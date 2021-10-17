cf729fd88c57379808670d6c7751cf3e496fa99f
dakeXd <47038301+dakeXd@users.noreply.github.com>
2021-10-17 17:33:07 +0200

    ScriptableCars Done

---
+++ TalesOFDumbgeon/Assets/Resources/Cards/CardsActions/SetHealth.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Cards.CardsActions
+{
+    [CreateAssetMenu(fileName = "New Card", menuName = "Cards/Effects/Health")]
+    public class SetHealth : CardEffect
+    {
+        public int cantidad;
+        public int objetivo;
+        public override string Start()
+        {
+            Debug.Log("Dmg: " + cantidad + "a" + objetivo);
+            return "";
+        }
+    }
+}
