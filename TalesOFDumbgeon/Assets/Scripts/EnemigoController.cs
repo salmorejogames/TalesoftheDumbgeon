82e359d322f2132ef3651f389ed3b7a0d6a7a8ba
strikealex <43806315+strikealex@users.noreply.github.com>
2021-10-19 18:59:51 +0200

    Mejora de la IA

--- TalesOFDumbgeon/Assets/Scripts/EnemigoController.cs
+++ TalesOFDumbgeon/Assets/Scripts/EnemigoController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemigoController : MonoBehaviour
 {
@@ -9,18 +10,18 @@ public class EnemigoController : MonoBehaviour
     public int velocidad = 5;
     public int armadura = 3;
     public int damage = 1;
-    public float vision = 10;
+    public float vision = 3;
 
     public GameObject personaje;
 
     private Rigidbody2D rb;
     private enum Estado { Wandering, Detected, Attacking};
     private Estado estadoActual = Estado.Wandering;
-    private Vector3 nextPos = Vector3.zero;
+    private Vector2 nextPos = Vector2.zero;
     private float distanciaPlayer;
     public enum tipoEnemigo {Abuesqueleto, Cerebro, Duonde, Palloto};
     public tipoEnemigo especie;
-    private float tiempoDesplazandose = 0;
+    private NavMeshAgent agente;
 
     private void Awake()
     {
@@ -31,45 +32,50 @@ public class EnemigoController : MonoBehaviour
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
-        personaje = GameObject.FindGameObjectWithTag("Player");        
+        personaje = GameObject.FindGameObjectWithTag("Player");
+        agente = GetComponent<NavMeshAgent>();
 
         if (especie == tipoEnemigo.Abuesqueleto)
         {
             velocidad = 1;
             armadura = 3;
             damage = 6;
-            vision = 10;
+            vision = 3;
         }
         else if (especie == tipoEnemigo.Cerebro)
         {
             velocidad = 6;
             armadura = 1;
             damage = 10;
-            vision = 10;
+            vision = 3;
         }
         else if (especie == tipoEnemigo.Duonde)
         {
             velocidad = 1;
             armadura = 15;
             damage = 3;
-            vision = 10;
+            vision = 3;
         }
         else if (especie == tipoEnemigo.Palloto)
         {
             velocidad = 15;
             armadura = 2;
             damage = 7;
-            vision = 10;
+            vision = 3;
         }
-        distanciaPlayer = Vector3.Distance(transform.position, personaje.transform.position);
+        distanciaPlayer = Vector2.Distance(transform.position, personaje.transform.position);
+        agente.updateRotation = false;
+        agente.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (personaje != null && tiempoDesplazandose <= 0)
+        gameObject.transform.position = Vector2.MoveTowards(transform.position, nextPos, velocidad * Time.deltaTime);
+
+        if (personaje != null && Vector2.Distance(transform.position, nextPos) < vision)
         {
-            distanciaPlayer = Vector3.Distance(transform.position, personaje.transform.position);
+            distanciaPlayer = Vector2.Distance(transform.position, personaje.transform.position);
             if (distanciaPlayer >= 3)
                 estadoActual = Estado.Wandering;
             else if (distanciaPlayer <= 3 && distanciaPlayer >= 0.5f)
@@ -85,7 +91,8 @@ public class EnemigoController : MonoBehaviour
                     break;
 
                 case Estado.Detected:
-                    nextPos = Vector3.MoveTowards(transform.position, personaje.transform.position, velocidad * Time.deltaTime);
+                    agente.SetDestination(personaje.transform.position);
+                    //transform.LookAt(personaje.transform.position);
                     break;
 
                 case Estado.Attacking:
@@ -93,9 +100,7 @@ public class EnemigoController : MonoBehaviour
                     break;
             }            
         }
-
-        gameObject.transform.position = Vector3.MoveTowards(transform.position, nextPos, velocidad * Time.deltaTime);
-        tiempoDesplazandose -= Time.deltaTime;
+        
     }
 
     private void CanAttack()
@@ -105,25 +110,7 @@ public class EnemigoController : MonoBehaviour
 
     private void Wander()
     {
-        tiempoDesplazandose = 5;
-        Vector3 randPos = Vector3.zero;
-        int dir = UnityEngine.Random.Range(1, 5);
-        if(dir == 1)
-        {            
-            nextPos = new Vector3(transform.position.x, transform.position.y + 100, transform.position.z);
-        }
-        else if (dir == 2)
-        {
-            nextPos = new Vector3(transform.position.x, transform.position.y - 100, transform.position.z);
-        }
-        else if(dir == 3)
-        {
-            nextPos = new Vector3(transform.position.x + 100, transform.position.y, transform.position.z);
-        }
-        else if (dir == 4)
-        {
-            nextPos = new Vector3(transform.position.x - 100, transform.position.y, transform.position.z);
-        }
+        nextPos = new Vector2(UnityEngine.Random.Range(-vision, vision), UnityEngine.Random.Range(-vision, vision));
     }
 
 
@@ -140,7 +127,7 @@ public class EnemigoController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            rb.velocity = Vector3.zero;
+            rb.velocity = Vector2.zero;
         }
     }
 
