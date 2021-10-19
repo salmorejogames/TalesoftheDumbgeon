ad6d629bb3e0cb1b11307c803cd2cfcc16df4de2
dakeXd <47038301+dakeXd@users.noreply.github.com>
2021-10-19 16:26:32 +0200

    Map transitions completed

--- TalesOFDumbgeon/Assets/Scripts/Map/MapInstance.cs
+++ TalesOFDumbgeon/Assets/Scripts/Map/MapInstance.cs
@@ -5,36 +5,11 @@ using UnityEngine;
 public class MapInstance : MonoBehaviour
 {
     public Vector2 dimensions;
-    private Vector3 _objetive;
-    public float gapX;
-    public float transitionSpeed;
-    private bool _ready = true;
-    [SerializeField] private Collider2D _mapTrigger;
-
-    public bool GetReady()
-    {
-        return _ready;
-    }
-
-    public void TransitionMap(Vector3 objetive)
-    {
-        _objetive = objetive;
-        StartCoroutine(nameof(Transition));
-    }
-
+    [SerializeField] private Collider2D mapTrigger;
+    
     public void SetTrigger(bool active)
     {
-        Debug.Log("Activado: " + active);
-        _mapTrigger.enabled = active;
+        mapTrigger.enabled = active;
     }
-
-    IEnumerator Transition()
-    {
-        while(Vector3.Distance(transform.position, _objetive) > 0.001)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, _objetive, transitionSpeed*Time.deltaTime);
-            yield return null;
-        }
-        _ready = true;
-    }
+    
 }
