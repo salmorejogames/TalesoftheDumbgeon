7089d9b5c581ff34c269f24e7b664bb7a18cf817
Donny <ismaeldnyjimenez@gmail.com>
2021-11-19 18:37:05 +0100

    Revert "Hot fix"
    This reverts commit 08a8350986a053e0e6fac749a3c8e502fbc3b1ae.

--- TalesOFDumbgeon/Assets/Scripts/Inteface/GameOverController.cs
+++ TalesOFDumbgeon/Assets/Scripts/Inteface/GameOverController.cs
@@ -5,11 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class GameOverController : MonoBehaviour
 {
-    [SerializeField] private GameObject blackPanel;
-    [SerializeField] private Animator blackPanelAnimator;
-
-    [SerializeField] private AudioSource gameOverAS;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -24,21 +19,11 @@ public class GameOverController : MonoBehaviour
 
     public void Reintentar()
     {
-        blackPanel.SetActive(true);
-        blackPanelAnimator.Play("FadeOut", 0, 0);
-
-        StartCoroutine(EsperarReintentar());
-        //SceneManager.LoadScene("IsometricScene");
+        SceneManager.LoadScene("IsometricScene");
     }
 
     public void Salir()
     {
         SceneManager.LoadScene("ClickParaEmpezar");
-    }
-
-    IEnumerator EsperarReintentar()
-    {
-        yield return new WaitForSeconds(1.5f);
-        SceneManager.LoadScene("IsometricScene");
     }
 }
